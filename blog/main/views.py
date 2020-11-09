5292ee3a91845617dc02e146b9073b4375ab0e01
Sergey K <serjkasmail@gmail.com>
2020-11-09 16:36:27 +0300

    create models

--- blog/main/views.py
+++ blog/main/views.py
@@ -3,13 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     data = {
-        'title': 'Главная страница',
-        'values': ['Some', 'Hello', '123'],
-        'obj': {
-            'car': 'BMW',
-            'age': 16,
-            'hobby': 'football'
-        }
+        'title': 'Главная страница'
     }
     return render(request, 'main/index.html', data)
 
